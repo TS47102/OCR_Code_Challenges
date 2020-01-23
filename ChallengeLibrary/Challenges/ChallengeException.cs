2ed820242c783cd33a4f121c966a85c1173aef24
Theo Stevens <TS47102@suffolkone.ac.uk>
2020-01-23 17:26:48 +0000

    Added custom exception type

---
+++ ChallengeLibrary/Challenges/ChallengeException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeLibrary.Challenges
+{
+	/// <summary>
+	/// The base class for all handleable exceptions thrown by Challenges.
+	/// </summary>
+	/// <remarks>This exception type (And any derived types) are to be used by Challenges when
+	/// the exception should be caught and suppressed by the caller, with little/no action required.</remarks>
+	public class ChallengeException : Exception
+	{
+		public ChallengeException () : base ()
+		{
+
+		}
+		
+		public ChallengeException (string message) : base (message)
+		{
+
+		}
+
+		public ChallengeException (string message, Exception inner) : base (message, inner)
+		{
+
+		}
+	}
+}
