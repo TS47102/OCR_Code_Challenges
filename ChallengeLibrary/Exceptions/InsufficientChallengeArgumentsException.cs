91648098530abef4141aff661d3140e37552b566
Pixelstorm <thepixelstormer@gmail.com>
2020-01-30 13:55:33 +0000

    Add InsufficientChallengeArgumentsException

---
+++ ChallengeLibrary/Exceptions/InsufficientChallengeArgumentsException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+using ChallengeLibrary.Reflection;
+
+namespace ChallengeLibrary.Exceptions
+{
+	/// <summary>
+	/// Indicates that an <see cref="IConsoleChallenge"/> has not been provided with enough arguments.
+	/// </summary>
+	[Serializable]
+	public class InsufficientChallengeArgumentsException : ChallengeException
+	{
+		public InsufficientChallengeArgumentsException () : base ()
+		{
+
+		}
+
+		public InsufficientChallengeArgumentsException (string message) : base (message)
+		{
+
+		}
+
+		public InsufficientChallengeArgumentsException (string message, Exception innerException) : base (message, innerException)
+		{
+
+		}
+
+		protected InsufficientChallengeArgumentsException (SerializationInfo info, StreamingContext context) : base (info, context)
+		{
+
+		}
+	}
+}
