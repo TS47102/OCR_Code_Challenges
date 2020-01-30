f2ca59947e61b266db3c352b6b447f5a9a23ebbf
Pixelstorm <thepixelstormer@gmail.com>
2020-01-30 14:02:19 +0000

    Added fields and extra constructors

--- ChallengeLibrary/Exceptions/InsufficientChallengeArgumentsException.cs
+++ ChallengeLibrary/Exceptions/InsufficientChallengeArgumentsException.cs
@@ -10,6 +10,9 @@ namespace ChallengeLibrary.Exceptions
 	[Serializable]
 	public class InsufficientChallengeArgumentsException : ChallengeException
 	{
+		public int providedArgumentCount { get; }
+		public int expectedArgumentCount { get; }
+
 		public InsufficientChallengeArgumentsException () : base ()
 		{
 
@@ -23,6 +26,30 @@ namespace ChallengeLibrary.Exceptions
 		public InsufficientChallengeArgumentsException (string message, Exception innerException) : base (message, innerException)
 		{
 
+		}
+
+		public InsufficientChallengeArgumentsException (int providedArgumentCount, int expectedArgumentCount) : base ($"Recieved {providedArgumentCount} arguments when {expectedArgumentCount} were expected.")
+		{
+			this.providedArgumentCount = providedArgumentCount;
+			this.expectedArgumentCount = expectedArgumentCount;
+		}
+
+		public InsufficientChallengeArgumentsException (int providedArgumentCount, int expectedArgumentCount, Exception innerException) : base ($"Recieved {providedArgumentCount} arguments when {expectedArgumentCount} were expected.", innerException)
+		{
+			this.providedArgumentCount = providedArgumentCount;
+			this.expectedArgumentCount = expectedArgumentCount;
+		}
+
+		public InsufficientChallengeArgumentsException (string message, int providedArgumentCount, int expectedArgumentCount) : base (message)
+		{
+			this.providedArgumentCount = providedArgumentCount;
+			this.expectedArgumentCount = expectedArgumentCount;
+		}
+
+		public InsufficientChallengeArgumentsException (string message, int providedArgumentCount, int expectedArgumentCount, Exception innerException) : base (message, innerException)
+		{
+			this.providedArgumentCount = providedArgumentCount;
+			this.expectedArgumentCount = expectedArgumentCount;
 		}
 
 		protected InsufficientChallengeArgumentsException (SerializationInfo info, StreamingContext context) : base (info, context)
