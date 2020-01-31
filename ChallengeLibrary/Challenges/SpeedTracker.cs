03a3367a19967056421c1833aaeb1c8eb7544f8e
Pixelstorm <thepixelstormer@gmail.com>
2020-01-31 21:53:24 +0000

    Update doc comment

--- ChallengeLibrary/Challenges/SpeedTracker.cs
+++ ChallengeLibrary/Challenges/SpeedTracker.cs
@@ -8,13 +8,15 @@ namespace ChallengeLibrary.Challenges
 	/// <summary>
 	/// Create a program that takes a time for a car going past a speed camera, the time going past the next one and the distance between them to calculate the average speed for the car
 	/// in mph.The cameras are one mile apart.
+	/// </summary>
+	/// <remarks>
 	/// Extensions:
 	/// 1. Speed cameras know the timings of each car going past, through number plate recognition. Valid number plates are two letters, two numbers and three letters afterwards, for
 	/// example XX77 787. Produce a part of the program that checks whether a number plate matches the given pattern. Tell the user either way.
 	/// 2. Create a program for creating a file of details for vehicles exceeding the speed limit set for a section of road. You will need to create a suitable file with test data, including
 	/// randomised number plates and times. You will then use the code you’ve already written to process this list to determine who is breaking the speed limit (70mph) and who has
 	/// invalid number plates.
-	/// </summary>
+	/// </remarks>
 	public class SpeedTracker : IConsoleChallenge
 	{
 		public const double CAMERA_DISTANCE_MILES = 1;
